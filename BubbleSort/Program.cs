a1091616f8d9646ffa3986a2bfbcc3024e3b14f9
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2021-06-23 01:58:28 -0500

    first version, algorithm completed

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -6,7 +6,49 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("BubbleSort");
+            Console.WriteLine("==========");
+
+            Console.WriteLine("Enter the lenght of your numbers list please:");
+            int len = Convert.ToInt32(Console.ReadLine());
+            int[] list = new int[len];
+            string input;
+
+            for(int i = 0; i < len; i++)
+            {
+                Console.WriteLine(string.Format("Enter the {0} index number of the list: ",i));
+                input = Console.ReadLine();
+                list[i] = Convert.ToInt32(input);                                  
+            }
+
+            Console.WriteLine("Results");
+            Console.WriteLine("=======");
+
+            int[] OrderedList = order(list);
+
+            foreach (var item in OrderedList)
+                Console.WriteLine(item.ToString());
+                        
+            Console.ReadLine();
+        }
+
+        private static int[] order(int[] List)
+        {
+            int temp;
+            for (int i = List.Length; i > 0; i--)
+            {                
+                for (int j = 0; j < i-1; j++)
+                {
+                    
+                    if (List[j] > List[j + 1])
+                    {
+                        temp = List[j+1];
+                        List[j + 1] = List[j];
+                        List[j] = temp;
+                    }                        
+                }
+            }
+            return List;
         }
     }
 }
